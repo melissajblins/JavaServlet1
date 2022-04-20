7bc5b80445763407b873bfe41daf61a6c691da4e
Melissa <meljblins@gmail.com>
2022-04-20 14:25:46 -0300

    Curso Java Servlet 1 finalizado

---
+++ gerenciador/src/main/java/gerenciador/OlaMundoServlet.java
@@ -0,0 +1,25 @@
+package gerenciador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/OlaMundo")
+public class OlaMundoServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Olá, Mundo!");
+        out.println("</body>");
+        out.println("</html>");
+        		
+	}
+}
